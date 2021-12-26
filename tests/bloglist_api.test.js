2b2d78b51f901294bd512837721a20872b7c368c
alexcarantona <info@alejandrocarantonna.es>
2021-12-26 20:40:58 +0100

    #4.9 (bloglist tests step2) Done
    _id field is transformed into id and __v is deleted from response

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -29,9 +29,15 @@ beforeEach(async() => {
 
 test('There are two blogs listed', async() => {
   const response = await api.get('/api/blogs');
-
   expect(response.body).toHaveLength(2)
 });
+
+test('The _id field is substituted by id property in returned JSON', async () => {
+  const response = await api.get('/api/blogs');
+  expect(response.body[0].id).toBeDefined();
+  expect(response.body[0]._id).not.toBeDefined();
+  expect(response.body[0].__v).not.toBeDefined();
+})
 
 afterAll(() => {
   mongoose.connection.close()
